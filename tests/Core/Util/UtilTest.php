0849bb62cd5fa8d81ae4c88851ebb816def0af7b
Alan Pich <alan.pich@gmail.com>
2014-03-07 16:31:26 +0000

    100% test coverage for Slender\Core\Util

---
+++ tests/Core/Util/UtilTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace Slender\Core\Util;
+
+interface MyInterfaceForTesting {}
+class MyClassForTesting implements MyInterfaceForTesting {}
+class MyOtherClassForTesting {}
+
+
+
+class UtilTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplementsInterface()
+    {
+        $interface = 'Slender\Core\Util\MyInterfaceForTesting';
+        $hasInterfaceClass = 'Slender\Core\Util\MyClassForTesting';
+        $noInterfaceClass  = 'Slender\Core\Util\MyOtherClassForTesting';
+        $util = new Util();
+
+        // Test works with string classnames
+        $this->assertTrue($util->implementsInterface($hasInterfaceClass,$interface));
+        $this->assertFalse($util->implementsInterface($noInterfaceClass,$interface));
+
+        // Test works with class instances
+        $this->assertTrue($util->implementsInterface(new $hasInterfaceClass,$interface));
+        $this->assertFalse($util->implementsInterface(new $noInterfaceClass,$interface));
+    }
+
+
+    public function testHyphenCase()
+    {
+        $expected = 'my-test-string';
+        $this->assertEquals($expected, Util::hyphenCase('MyTestString'));
+    }
+
+    public function testSetterMethodName()
+    {
+        $this->assertEquals('setMyThing',Util::setterMethodName('myThing'));
+    }
+} 
